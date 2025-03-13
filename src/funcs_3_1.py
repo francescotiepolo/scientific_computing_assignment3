6046119dc021000e433e0da61159e2f9043714c3
francescotiepolo <francescotiepolo@icloud.com>
2025-03-13 16:40:47 +0100

    added markdowns everywhere

--- src/funcs_3_1.py
+++ src/funcs_3_1.py
@@ -50,8 +50,8 @@ def M_matrix_circle(N, L):
         M: scipy.sparse.csr_matrix, matrix M.
     '''
     h = L / (N + 1) # Step size
-    x = np.linspace(0, L, N) # Grid points x-axis
-    y = np.linspace(0, L, N) # Grid points y-axis
+    x = np.linspace(h, L - h, N) # Grid points x-axis
+    y = np.linspace(h, L - h, N) # Grid points y-axis
     X, Y = np.meshgrid(x, y)
 
     M = M_matrix_square(N) # Generate matrix M for square domain
@@ -68,6 +68,15 @@ def M_matrix_circle(N, L):
     return M.tocsr(), X, Y, not_circle
 
 def obtain_sort_solutions(M, n=3):
+    ''' Obtain the n solutions with the smallest(closest to 0) eigenvalues of the 2D wave equation.
+    Input:
+        M: scipy.sparse.csr_matrix, matrix M.
+        n: int, number of solutions to obtain.
+    Output:
+        eigvals: numpy.ndarray, eigenvalues.
+        eigvecs: numpy.ndarray, eigenvectors.
+        lowest_sols: numpy.ndarray, indices of the n solutions with the smallest eigenvalues.
+    '''
     eigvals, eigvecs = scipy.linalg.eigh(M) # Solve the eigenvalue problem
     # Sort eigenvalues/eigenvectors
     idx = np.argsort(eigvals)
@@ -78,29 +87,46 @@ def obtain_sort_solutions(M, n=3):
     lowest_sols = neg_indices[-n:]
     return eigvals, eigvecs, lowest_sols
 
-def min_max_values(eigvecs, lowest_modes, Nx, Ny):
-    global_min = np.inf
-    global_max = -np.inf
-    for i in lowest_modes:
+def min_max_values(eigvecs, lowest_sols, Nx, Ny):
+    ''' Obtain the minimum and maximum values of the solutions.
+    Input:
+        eigvecs: numpy.ndarray, eigenvectors.
+        lowest_modes: numpy.ndarray, indices of the solutions we want to plot.
+        Nx: int, steps in the x-axis.
+        Ny: int, steps in the y-axis.
+    Output:
+        min: float, minimum value.
+        max: float, maximum value.
+    '''
+    min_v = np.inf
+    max_v = -np.inf
+    for i in lowest_sols: # Loop over the solutions and find the minimum and maximum values
         sol = eigvecs[:, i].reshape(Ny, Nx)
-        global_min = min(global_min, sol.min())
-        global_max = max(global_max, sol.max())
-    return global_min, global_max
+        min_v = min(min_v, sol.min())
+        max_v = max(max_v, sol.max())
+    return min_v, max_v
 
 def measure_time(N, runs=10):
+    ''' Measure the time it takes to solve the eigenvalue problem for the 2D wave equation.
+    Input:
+        N: int, number of spatial steps.
+        runs: int, number of desired runs from which compute average.
+    Output:
+        dict, containing the mean and confidence interval for the dense and sparse average time.
+    '''
     time_dense=[]
     time_sparse=[]
 
-    for _ in range(runs):
-        M = M_matrix_square(N)
+    for _ in range(runs): # Loop over the number of runs
+        M = M_matrix_square(N) # Generate matrix M
         M_dense = M.toarray()
 
         start = time.time()
-        scipy.linalg.eigh(M_dense)
+        scipy.linalg.eigh(M_dense) # Solve the eigenvalue problem with dense solver
         time_dense.append(time.time() - start)
 
         start = time.time()
-        sp.linalg.eigs(M)
+        sp.linalg.eigs(M) # Solve the eigenvalue problem with sparse solver
         time_sparse.append(time.time() - start)
     
     return {'dense mean': np.mean(time_dense),
@@ -109,8 +135,16 @@ def measure_time(N, runs=10):
             'sparse ci': 1.96 * np.std(time_sparse, ddof=1) / np.sqrt(runs)}
 
 def compute_freq(N, L, k=10):
+    '''Compute k largest frequencies for the 2D wave equation in a square domain.
+    Input:
+        N: int, number of spatial steps.
+        L: float, length of the square domain.
+        k: int, number of frequencies to compute.
+    Output:
+        numpy.ndarray, k frequencies.
+    '''
     h = L / (N + 1)
-    M = M_matrix_square(N)
-    eigenvalues, _ = sp.linalg.eigs(M, k)
-    frequencies = np.sqrt(np.abs(eigenvalues)) / h
+    M = M_matrix_square(N) # Generate matrix M
+    eigenvalues, _ = sp.linalg.eigs(M, k) # Solve the eigenvalue problem obtaining k largest eigenvalues
+    frequencies = np.sqrt(np.abs(eigenvalues)) / h # Compute corresponding frequencies
     return np.sort(frequencies)
